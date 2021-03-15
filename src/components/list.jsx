4990c7212ccf7793dae12f66f9acdf1f13d1afd8
Elijahscriptdev <elijahobominuru@gmail.com>
2021-03-15 11:53:05 +0100

    fixed warning error

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function List({ listItem, key, handleHover, handleHoverOut }) {
+export default function List({ listItem, handleHover, handleHoverOut }) {
   return (
     <>
       <li
         className='list-item'
-        key={key}
         onClick={handleHover}
         onMouseOut={handleHoverOut}
       >
